c4c6924a1cc2afda9034297e34c83605946d66c8
Clark0 <caoliu98@yahoo.com>
2024-10-13 11:42:18 +0800

    switch to lazyvim

--- home-manager/config/nvim/lua/mappings.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.mappings"
-
--- add yours here
-
-local map = vim.keymap.set
-
-map("i", "jj", "<ESC>")
-map("n", "<leader>a", "ggVG", { desc = "Select all" })
-map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlight" })
